4ad13eef07851df7ee24ed0fb18c2a7d9bdaf37f
Weiliang Xie <weiliangxie@hotmail.com>
2020-10-10 17:07:26 -0400

    time travel test

---
+++ In a Timely Manor/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float smoothing = 0.6f; // How smoothly camera follows target.
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, target.position.z - 10);
+
+        // Smoothly follow target.
+        if(transform.position != target.position)
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);        
+        
+    }
+}
---
+++ In a Timely Manor/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 5.0f;
+
+    [SerializeField]
+    bool inPast = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Time travel.
+        if (Input.GetButtonDown("TimeShift"))
+            TimeTravel();
+        
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    void TimeTravel() {
+
+        float travel = 0;
+
+        // Go from past to present.
+        if (!inPast) {
+            travel = 15;
+            inPast = true;
+        }
+        else {
+            travel = -15;
+            inPast = false;
+        }
+
+        this.transform.position += new Vector3(0, 0, travel); // Unity doesn't allow you to only modify the z value...
+    }
+}
