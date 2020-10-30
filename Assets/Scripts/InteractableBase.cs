6ce24d03fdc926f04dee9514fccb27e2c2beb7c3
jaredcohen1998 <32070535+jaredcohen1998@users.noreply.github.com>
2020-10-30 18:07:59 -0400

    Added interactionSystem stuff manually

---
+++ Assets/Scripts/InteractableBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    protected Inventory inventory;
+
+    public abstract void OnInteraction();
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetButton("Interact"))
+            {
+                OnInteraction();
+            }
+        }
+    }
+}
+
+public class InteractableBase : MonoBehaviour
+{
+    
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -8,9 +8,42 @@ public class Inventory : MonoBehaviour
     public List<string> items;
     public Image[] slots;
     [SerializeField]
-    GameObject invent;
+    GameObject invent = null;
 
     private void Start() {
         slots = invent.GetComponentsInChildren<Image>();
+    }
+
+    // add an item to the inventory
+    public void AddItem(GameObject obj)
+    {
+        items.Add(obj.name); // Add item name to inventory
+        Sprite sprite = obj.GetComponent<SpriteRenderer>().sprite;
+
+        // Change empty slot to object image
+        foreach (Image img in slots)
+        {
+            if (img.sprite.name == "empty_slot")
+            {
+                img.sprite = sprite;
+                break;
+            }
+        }
+
+        Destroy(obj); // Object not in game world anymore
+    }
+
+    // check if inventory contains a specific item
+    public bool ContainsItem(GameObject obj)
+    {
+        foreach (string s in items)
+        {
+            if (s == obj.name)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
