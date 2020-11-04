2a34bbbb9954ee7b72c2bb53d1f1ca003ff0520f
Weiliang Xie <weiliangxie@hotmail.com>
2020-11-03 22:27:15 -0500

    fixed tree and added door colliders

---
+++ Assets/Scripts/InteractableLockedDoor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableLockedDoor : Interactable
+{
+    public override void OnInteraction() {
+        MessageController.ShowMessage("The door isn't budging... Seems to be locked.");
+    }
+}
