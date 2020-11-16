6d5dbb01e561c8c4dd7e1568f1064755f3e94de9
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-11-15 22:11:19 -0500

    Level 2 Present

---
+++ Assets/Scripts/Interactables/InteractableBed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBed : Interactable
+{
+    public override void OnInteraction() {
+        MessageController.ShowMessage("It's not time to go to sleep yet.");
+        canInteract = false;
+    }
+}
