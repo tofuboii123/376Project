82453ca20caaf0b7ac5c6ad20b4891f4c81519e9
jaredcohen1998 <32070535+jaredcohen1998@users.noreply.github.com>
2020-11-25 17:08:31 -0500

    Merge stuff

---
+++ Assets/Scripts/NoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoDestroy : MonoBehaviour {
+    public string tagName;
+    void Awake() {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(tagName);
+        if (objects.Length > 1) {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
